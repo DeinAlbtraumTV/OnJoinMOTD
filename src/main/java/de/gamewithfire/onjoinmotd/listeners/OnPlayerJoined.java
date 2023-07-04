077c75ade6e640671eacbf367ebe9b63a4d84e27
DeinAlbtraumTV <jaspi2004@gmail.com>
2023-07-04 17:50:06 +0200

    Changed event to PlayerJoinEvent Removed additional logging

--- src/main/java/de/gamewithfire/onjoinmotd/listeners/OnPlayerJoined.java
+++ src/main/java/de/gamewithfire/onjoinmotd/listeners/OnPlayerJoined.java
@@ -1,12 +1,13 @@
 package de.gamewithfire.onjoinmotd.listeners;
 
 import de.gamewithfire.onjoinmotd.OnJoinMotd;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * Listener Class for PlayerLoginEvent.
@@ -20,7 +21,7 @@ public class OnPlayerJoined implements Listener {
   }
 
   @EventHandler(priority = EventPriority.NORMAL)
-  public void onPlayerLoginEvent(PlayerLoginEvent event) {
-    event.getPlayer().sendMessage(NamedTextColor.GOLD + (String) config.get("motd"));
+  public void onPlayerJoinEvent(PlayerJoinEvent event) {
+    event.getPlayer().sendMessage(Component.text((String) config.get("motd"), NamedTextColor.GOLD));
   }
 }
